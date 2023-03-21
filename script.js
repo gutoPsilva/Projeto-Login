4cb9fb2ede33f603eff13ab9c077cb50b9be4580
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-03-21 10:40:30 -0300

    Issues fix

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ let root = document.querySelector(':root')
 // rs.getPropertyValue('--nomeVar') --> pegar variável CSS, necessário criar outra variável RS
 // r.style.setProperty('--nomeVar', '#ffffff') --> alterar variável CSS
 
-// manhã, tarde e madrugada. O Site já foi desenvolvido com o tema noite.
-if (hora>4 && hora<=12){
+// madrugada, manhã e tarde. O Site já foi desenvolvido com o tema noite.
+if(hora<=4){
+    background.style.backgroundImage = "url(images/backgroundMadrugada.jpg"
+    root.style.setProperty('--priCor', '#0E1E2D')
+    root.style.setProperty('--segCor', '#132831')
+    root.style.setProperty('--terCor', '#203A43')
+}
+else if (hora<=12){
     background.style.backgroundImage = "url(images/backgroundManha.jpg)"
     root.style.setProperty('--priCor', '#4346B6')
     root.style.setProperty('--segCor', '#413c94')
-    root.style.setProperty('terCor', '#7A5BC9')
+    root.style.setProperty('--terCor', '#7A5BC9')
 }
-else if (hora>12 && hora<=16){
+else if (hora<=18){
     background.style.backgroundImage = "url(images/backgroundTarde.jpg"
     root.style.setProperty('--priCor', '#1D2671')
     root.style.setProperty('--segCor', "#4f4e94")
     root.style.setProperty('--terCor', '#65417a')
-}
-else if(hora>=0 && hora<=4){
-    background.style.backgroundImage = "url(images/backgroundNoite.jpg"
-    root.style.setProperty('--priCor', '#0E1E2D')
-    root.style.setProperty('--segCor', '#132831')
-    root.style.setProperty('--terCor', '#203A43')
 }
