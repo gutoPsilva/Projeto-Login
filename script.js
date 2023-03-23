5f39c43d4b57040cb9572a41423a9b0a414c82e8
gutoPsilva <60670589+gutoPsilva@users.noreply.github.com>
2023-03-23 19:51:40 -0300

    FlexBox e MQ p/ ajustes

--- script.js
+++ script.js
@@ -6,21 +6,24 @@ let root = document.querySelector(':root')
 // r.style.setProperty('--nomeVar', '#ffffff') --> alterar variável CSS
 
 // madrugada, manhã e tarde. O Site já foi desenvolvido com o tema noite.
-if(hora<=4){
+hora = 19
+if(hora<=6){
     background.style.backgroundImage = "url(images/backgroundMadrugada.jpg"
     root.style.setProperty('--priCor', '#0E1E2D')
     root.style.setProperty('--segCor', '#132831')
     root.style.setProperty('--terCor', '#203A43')
 }
-else if (hora<=12){
+else if (hora<=18){
     background.style.backgroundImage = "url(images/backgroundManha.jpg)"
     root.style.setProperty('--priCor', '#4346B6')
     root.style.setProperty('--segCor', '#413c94')
     root.style.setProperty('--terCor', '#7A5BC9')
 }
+/* 
 else if (hora<=18){
     background.style.backgroundImage = "url(images/backgroundTarde.jpg"
     root.style.setProperty('--priCor', '#1D2671')
     root.style.setProperty('--segCor', "#4f4e94")
     root.style.setProperty('--terCor', '#65417a')
 }
+ feio p crl o tema tarde*/
